ea4a8ce2538064e8decb2fcbf28099ddc6f55d92
gitahievans <evanygk@gmail.com>
2022-10-13 15:07:19 +0300

    schema changed

--- db/migrate/20221013075440_drop_landlords.rb
+++
@@ -1,5 +0,0 @@
-class DropLandlords < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :landlords
-  end
-end
--- db/migrate/20221013075454_drop_houses.rb
+++
@@ -1,5 +0,0 @@
-class DropHouses < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :houses
-  end
-end
