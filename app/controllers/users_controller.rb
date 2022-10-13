198ea7422816c275ab3c71a4f9229b950b719f1a
elijahbundi <bundielly@gmail.com>
2022-10-13 11:28:23 +0300

    Controller updated successfully

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord;:RecordInvalid, with: :render_unprocessable_entity_response
+
+    def create_account
+        user = User.create!(user_params)
+        session[:user_id] ||= user.id
+        session[:user_type] ||= user.user_type
+        render json: user
+    end
+
+    def login
+        user = User.find_by(email: params[:email])
+        if user&.authenticate(params[:password])
+            session[:user_id] ||= user.id
+            session[:user_type] ||= user.user_type
+            render json: user, status: :created
+        else
+            render json: {errors: [error: "Invalid User"]}, status: :not_found
+        end
+    end
+
+    def logout
+        session.delete :user_id
+        session.delete :user_type
+        head :no_content
+    end
+
+    private
+
+    def user_params
+        params.permit(:name, :email, :password, :password_confirmation, :bio)
+    end
+
+    def render_unprocessable_entity_response(e)
+        render json: {errors: e.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
+end
